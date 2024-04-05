226ce9664d93056a1d5b450b0f9def2dd7da6ace
sneakypanda64 <alexander.j.heather@gmail.com>
2024-04-05 18:05:06 +0100

    .

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("sup")
+	})
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", "4000")))
+}
